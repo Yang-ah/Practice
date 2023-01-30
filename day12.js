de9865eb4623f607a76c032cb4dd14b09665c96a
Yang-ah <yangah0409@gmail.com>
2023-01-30 17:39:18 +0900

    practice reduce

--- day12.js
+++ day12.js
@@ -3,7 +3,7 @@
 // 2. 함수 : 객체의 value의 데이터 타입이 number인 [key, value]만 뽑아서 새로운 객체 return (reduce 사용)
 
 const cat = {
-  name: "kiity",
+  name: "kitty",
   age: 2,
   adopted: true,
   weight: 4,
@@ -14,24 +14,13 @@ const cat = {
   grooming: () => console.log("kitty is grooming"),
 };
 
-let catNum = {};
-
-// forEach 활용
-Object.entries(cat).forEach(([key, value]) => {
-  if (typeof value === "number") {
-    catNum[key] = value;
-  }
-});
-
-console.log(catNum);
-
 // reduce 활용
-let catNum2 = {};
 
-Object.entries(cat).reduce((acc, [key, value]) => {
+const result = Object.entries(cat).reduce((acc, [key, value]) => {
   if (typeof value === "number") {
-    catNum2[key] = value;
+    acc[key] = value;
   }
-});
+  return acc;
+}, {});
 
-console.log(catNum2);
+console.log(result);
